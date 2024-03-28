84e5b2ada129de6cb6d7416aae278df9b7495443
Harry Sun <syfsyf@me.com>
2024-03-28 20:28:04 +0800

    improve err handle

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,8 @@ export default {
 /**
  * Fetches the favicon for a given URL and size, using Google's favicon API first, if error try the page's HTML.
  * @param request 
- * @returns icon file if success, otherwise redirect, if error redirect to default favicon.ico
+ * @returns icon file if success, otherwise redirect, if error rturn to defaulticon
  */
-const defaultIconUrl = "https://he.net/favicon.ico";
 async function handleRequest(request: Request): Promise<Response> {
     let params;
     try { // convert param
@@ -36,21 +35,21 @@ async function handleRequest(request: Request): Promise<Response> {
     } catch (error) {
         console.error("param error: " + error);
         // If the parameters are invalid, redirect to the default icon.
-        return Response.redirect(defaultIconUrl, 307);
+        return defaultSvgicon();
     }
     try { // fetch icon use google api
         return await fetchIconUseGoogleApi(params.targetSize, params.targetUrl);
     } catch (error) {
-        console.warn("fetch Icon Use Google Api error: " + error);
+        console.warn("fetch Icon Use Google Api failed: " + error);
     }
     try { // fetch favicon from page
         return await fetchFaviconFromPage(params.targetSize, params.targetUrl);
     } catch (error) {
-        console.warn("fetch Favicon From Page error: " + error);
+        console.warn("fetch Favicon From Page failed: " + error);
     }
-    // If all attempts failed, redirect to the default icon.
-    console.error("All attempts failed, redirect to default icon.");
-    return Response.redirect(defaultIconUrl, 307);
+    // If all attempts failed, redirect to google api.
+    console.error("All attempts failed, redirect to google api.");
+    return Response.redirect(constructGoogleApiUrl(params.targetSize, params.targetUrl), 307);
 }
 
 async function convertParam(url: URL): Promise<{ targetSize: string; targetUrl: URL }> {
@@ -108,8 +107,8 @@ async function fetchIconUseGoogleApi(targetSize: string, targetUrl: URL): Promis
         } else {
             if (googleResponse.status === 404) {
                 // ERROR: The favicon was not found. 
-                console.error(`google favicon api 404.`);
-                throw new Error(`google favicon api 404.`);
+                // console.error(`google favicon api 404.`);
+                throw 404;
                 // return Response.redirect("https://he.net/favicon.ico", 307);
             } else {
                 // ERROR: Log the error for debugging purposes
@@ -120,7 +119,12 @@ async function fetchIconUseGoogleApi(targetSize: string, targetUrl: URL): Promis
         }
     } catch (e) {
         // console.error(`Failed to fetch favicon for ${targetUrl}: ${e}`);
-        throw new Error(`Failed to fetch favicon for ${targetUrl}: ${e}`);
+        if (e === 404) {
+            throw new Error(`Failed for ${targetUrl}: 404 not found.`);
+        } else {
+            return new Response(constructGoogleApiUrl(targetSize, targetUrl), { status: 307 })
+        }
+
     }
 }
 
@@ -175,8 +179,8 @@ async function fetchFaviconFromPage(targetSize: string, targetUrl: URL): Promise
         }
     }
     if (faviconUrlList.length === 0) {
-        console.warn("No favicon link found, the page text 0-200: " + html.slice(0, 200) + "...");
-        throw new Error("No favicon link found");
+        console.warn(`No favicon link found, the page text 0-200: ${html.slice(0, 200)}`);
+        throw new Error(`No favicon link found on page ${targetUrl}`);
     } else {
         return fetchFaviconUrlList(faviconUrlList)
     }
@@ -217,6 +221,23 @@ async function fetchFaviconUrlList(faviconUrlList: string[]) {
         return firstValidImage;
     } catch (error) {
         // If all promises are rejected, Promise.any will throw an AggregateError
-        throw new Error("No valid favicon found, either paage has no link or all fetches failed");
+        throw new Error("No valid favicon found, all fetches failed");
     }
+}
+
+async function defaultSvgicon() {
+    const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-square-fill" viewBox="0 0 16 16">
+    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927"/>
+  </svg>`;
+
+    const headers = {
+        'Content-Type': 'image/svg+xml',
+        // Cache for 1 hr
+        'Cache-Control': 'public, max-age=3600',
+    };
+
+    return new Response(svgContent, {
+        headers: headers,
+        status: 200 // OK status
+    });
 }
