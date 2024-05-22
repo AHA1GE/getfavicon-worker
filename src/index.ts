70831f413cefa244ad5e3b7266643b93bf0f492f
Harry Sun <syfsyf@me.com>
2024-05-22 16:58:50 +0800

    use icon horse instead

--- src/index.ts
+++ src/index.ts
@@ -95,6 +95,26 @@ async function convertParam(url: URL): Promise<{ targetSize: string; targetUrl: 
 }
 
 async function fetchIconUseGoogleApi(targetSize: string, targetUrl: URL): Promise<Response> {
+    function constructGoogleApiUrl(targetSize: string, targetUrl: URL): string {
+        // const googleApiBaseUrl = "https://t0.gstatic.com/faviconV2";
+        // const queryParams = new URLSearchParams({
+        //     client: 'chrome_desktop',
+        //     nfrp: '2',
+        //     check_seen: 'true',
+        //     size: targetSize,
+        //     min_size: '16',
+        //     max_size: '256',
+        //     url: targetUrl.toString(),
+        // });
+        const googleApiBaseUrl = "https://icon.horse/icon/";
+        const queryParams = new URLSearchParams({
+            // size: targetSize,
+            uri: targetUrl.toString(),
+        });
+        const googleApiUrl = `${googleApiBaseUrl}?${queryParams}`;
+        return googleApiUrl
+    }
+
     try {
         // Fetch the favicon from Google's API.
         const googleResponse = await fetch(constructGoogleApiUrl(targetSize, targetUrl));
@@ -134,20 +154,7 @@ async function fetchIconUseGoogleApi(targetSize: string, targetUrl: URL): Promis
     }
 }
 
-function constructGoogleApiUrl(targetSize: string, targetUrl: URL): string {
-    const googleApiBaseUrl = "https://t0.gstatic.com/faviconV2";
-    const queryParams = new URLSearchParams({
-        client: 'chrome_desktop',
-        nfrp: '2',
-        check_seen: 'true',
-        size: targetSize,
-        min_size: '16',
-        max_size: '256',
-        url: targetUrl.toString(),
-    });
-    const googleApiUrl = `${googleApiBaseUrl}?${queryParams}`;
-    return googleApiUrl
-}
+
 
 /** Fetches the favicon from page specified url.
     * @param targetSize The desired size of the favicon.
@@ -231,7 +238,7 @@ async function fetchFaviconUrlList(faviconUrlList: string[]) {
 }
 
 async function defaultSvgicon() {
-    const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-square-fill" viewBox="0 0 16 16">
+    const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-question-square-fill" viewBox="0 0 16 16">
     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927"/>
   </svg>`;
 
