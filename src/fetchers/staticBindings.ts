6570ed3e3cfce6e51b307fd960f0652882a707c7
Harry Sun <syfsyf@me.com>
2024-06-30 15:04:19 +0800

    refactor: Improve static favicon binding logic and error handling

--- src/fetchers/staticBindings.ts
+++ src/fetchers/staticBindings.ts
@@ -5,27 +5,29 @@ async function fetchIconBasedOnEnvVar(targetSize: string, targetUrl: URL, env: E
     const bindings = env.staticFaviconBindings;
     // bindinds = { "matchtargeturl.com" : "https://fetch.from.here/favicon.ico", "anothermatch.com": "https://fetch.from.here/favicon.ico}
     // if domain matches, fetch the icon from the binding's value, otherwise no match, reject the promise with "no static favicon binding matched"
-    let newTargetUrl;
+
+    const matchedUrl = bindings[targetUrl.hostname];
+
+    if (!matchedUrl) { return Promise.reject("no static favicon binding matched"); }
+
+    const newTargetUrl = new URL(matchedUrl);
+    console.log(`fetching icon from static binding: {'${targetUrl.hostname}':'${newTargetUrl}'}, converting to ${targetSize}x${targetSize}...`);
+
     try {
-        newTargetUrl = new URL(bindings[targetUrl.hostname]);
-    } catch (e) {
-        return Promise.reject("no static favicon binding matched");
-    }
-    try {
-        console.log(`fetching icon from static binding: {'${targetUrl.hostname}':'${newTargetUrl}'}, converting to ${targetSize}x${targetSize}...`);
         const targetSizeNum = parseInt(targetSize, 10);
         const bindingResponse = await fetch(new Request(newTargetUrl.toString()), { cf: { image: { format: "webp", height: targetSizeNum, width: targetSizeNum, fit: "contain" } } });
-        if (bindingResponse.ok) {
-            const contentType = bindingResponse.headers.get("Content-Type") || "image/x-icon";
-            if (contentType.startsWith("image/")) {
-                // SUCCESS: Return the fetched icon.
-                return resWithNewHeaders(bindingResponse);
-            } else {
-                throw new Error(`invalid Content-Type received: ${contentType}, url: ${newTargetUrl}`);
-            }
-        } else {
+
+        if (!bindingResponse.ok) {
             throw new Error(`status: ${bindingResponse.status}, url ${newTargetUrl}`);
         }
+
+        const contentType = bindingResponse.headers.get("Content-Type") || "image/x-icon";
+        if (!contentType.startsWith("image/")) {
+            throw new Error(`invalid Content-Type received: ${contentType}, url: ${newTargetUrl}`);
+        }
+
+        // SUCCESS: Return the fetched icon with new headers.
+        return resWithNewHeaders(bindingResponse);
     } catch (e) {
         throw e;
     }
